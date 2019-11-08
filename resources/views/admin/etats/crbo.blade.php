9366607fc091cf601870bbb5194c8286034046ae
abdiaschafy <abdiaschafang@gmail.com>
2019-11-08 13:39:44 +0100

    ss

--- resources/views/admin/etats/crbo.blade.php
+++ resources/views/admin/etats/crbo.blade.php
@@ -22,6 +22,9 @@
         margin-top: -10px !important;
         margin-right: 50px !important;
     }
+    .type_interve {
+        margin-top: -50px !important;
+    }
     .footer {
         padding-top: 1px;
         padding-bottom: 80px;
@@ -53,9 +56,9 @@
 
     <p class="force"><h4 class="h4"><u>COMPTE-RENDU OPERATOIRE</u></h4></p>
 
-    <div class="row col-md-5 offset-2">
+    <div class="row col-md-5 offset-3">
         <div class="row">
-            <p>Concernant le patient {{ $patient->name }} {{ $patient->prenom }} pour <b>{{ $patient->compte_rendu_bloc_operatoires->last()->titre_intervention }}</b></p>
+            <p>CONCERNANT LE PATIENT : <b>{{ $patient->name }} {{ $patient->prenom }}</b></p>
         </div>
     </div>
 
@@ -66,7 +69,7 @@
             <span><small><b>Anesthesiste:</b> {{ $patient->compte_rendu_bloc_operatoires->last()->anesthesiste }}</small></span><br>
             <span><small><b>Infirmier anesthesiste:</b> Dr. {{ $patient->compte_rendu_bloc_operatoires->last()->anesthesiste }}</small></span>
         </div>
-        <div class="col-md-3 offset-6">
+        <div class="col-md-2 offset-7">
             <span><small><b>DATE D'ENTREE :</b> {{ $patient->compte_rendu_bloc_operatoires->last()->date_e }}</small></span><br>
             <span><small><b>TYPE D'ENTREE :</b> {{ $patient->compte_rendu_bloc_operatoires->last()->type_e }}</small></span><br>
             <span><small><b>DATE DE SORTIE :</b> {{ $patient->compte_rendu_bloc_operatoires->last()->date_s }}</small></span><br>
@@ -74,7 +77,7 @@
         </div>
     </div>
 
-    <h6 class="text-"><u>TYPE D'INTERVENTION :</u></h6>
+    <h6 class="type_interve"><u>TYPE D'INTERVENTION :</u></h6>
     <div class="">
         <p>
             <b>{!! nl2br(e($patient->consultations->last()->type_intervention)) !!}</b>
